88e11d1683420606b65ede411d0c69681ccf96a1
Abhinav <abhinav4848@gmail.com>
2019-05-08 00:13:11 +0530

    more documentation

---
+++ Notes/config.py
@@ -0,0 +1,8 @@
+DATAFB_USERNAME = 'Abhinav'
+DATAFB_PASSWORD = 'Strong Pass'
+COOL_STUFF = 'hi'
+
+MYSQL = {'host': 'localhost',
+         'user': 'root',
+         'passwd': 'my secret password',
+         'db': 'write-math'}
