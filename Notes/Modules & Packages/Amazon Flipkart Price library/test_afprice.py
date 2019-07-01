d757be53d5affec89743b48fe831a917138853ba
Abhinav <abhinav4848@gmail.com>
2019-07-02 02:23:52 +0530

    random

--- Notes/Modules & Packages/Amazon Flipkart Price library/test_afprice.py
+++ Notes/Modules & Packages/Amazon Flipkart Price library/test_afprice.py
@@ -1,15 +1,16 @@
+import tqdm
 import afprice
 
 urls = ['https://www.amazon.in/dp/B07M9FDGJQ/', 
 'http://dl.flipkart.com/dl/honor-9n-sapphire-blue-64-gb/p/itmf9pgsehv6nmss']
 
 print("Using returns")
-for url in urls:
-    print(afprice.main(url, True))
-    print()
+returns = []
+for url in tqdm.tqdm(urls):
+    returns+=[afprice.main(url, True)]
+print(returns)
 
 print("Using prints")
 for url in urls:
     afprice.main(url, False)
     print()
-
